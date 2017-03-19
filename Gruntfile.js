30f1571dcd5a7df2805050b11f3da45ccb0fadb7
Steve Kaufman <skaufman@laughlin.com>
2017-03-18 19:12:27 -0500

    js cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,41 @@
 module.exports = function (grunt) {
-
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		contentDir: 'content/',
-		jsSrc: [
-			'<%= contentDir %>js/**/*.js',
-			//excludes
-			'!<%= contentDir %>js/_libs/**/*.js'
-		],
+		srcDir: 'content/assets/',
+		distDir: 'content/public/',
 		sassSrc: [
 			'<%= contentDir %>assets/styles/*.scss',
 			//excludes
 			'!<%= contentDir %>assets/styles/*/*.scss',
 			'!<%= contentDir %>assets/styles/_*.scss'
 		],
+
+		browserify: {
+			dist: {
+				options: {
+					transform: [['babelify', { presets: 'es2015' }]],
+					browserifyOptions: {
+						debug: true
+					}
+				},
+				files: {
+					'<%= distDir %>js/site.js': '<%= srcDir %>scripts/site.js'
+				}
+			}
+		},
+
+		uglify: {
+			my_target: {
+				options: {
+					sourceMap: true,
+					quoteStyle: 1,
+				},
+				files: {
+					'<%= distDir %>js/site.js': '<%= distDir %>js/site.js'
+				}
+			}
+		},
 
 		sass: {
 			dist: {
@@ -23,20 +45,39 @@ module.exports = function (grunt) {
 				expand: true,
 				flatten: true,
 				src: '<%= sassSrc %>',
-				dest: '<%= contentDir %>public/css/',
+				dest: '<%= distDir %>css/',
 				ext: '.css'
 			}
 		},
 
+		postcss: {
+			options: {
+				processors: [
+					require('autoprefixer') ({
+						browsers: ['last 2 versions']
+					})
+				]
+			},
+			dist: {
+				src: '<%= distDir %>css/site.css'
+			}
+		},
+
 		watch: {
-			sass: {
-				files: '<%= contentDir %>assets/styles/*.scss',
-				tasks: 'sass:dist'
+			css: {
+				files: ['<%= srcDir %>styles/**/*.scss'],
+				tasks: ['sass', 'postcss']
+			},
+			js: {
+				files: ['Gruntfile.js', '<%= srcDir %>/**/*.js'],
+				tasks: ['browserify', 'uglify']
 			}
 		}
-
 	});
 
+	grunt.loadNpmTasks('grunt-browserify');
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-postcss');
 };
